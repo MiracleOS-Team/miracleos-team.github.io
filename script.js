9af599a5be8cb5c0c9cc8a40f09bfaddd7e0f1c3
Teresa <teresanmegea@hotmail.com>
2025-01-09 14:46:29 -0300

    FrontPage about 70% done

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.getElementById("download").onclick = function(){
-    location.href = "download/index.html";
+document.getElementById("download").onclick = function () {
+  location.href = "download/index.html";
+};
+document.getElementById("learn-more").onclick = function () {
+  location.href = "features/index.html";
 };

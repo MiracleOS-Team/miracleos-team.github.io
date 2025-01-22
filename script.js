414263ad631187a4c3e1c0befcd346f0db46b507
Teresa <teresanmegea@hotmail.com>
2025-01-22 18:08:26 -0300

    Fixed the dropdown

--- script.js
+++ script.js
@@ -8,11 +8,7 @@ document.getElementById("home-mobile-btn").onclick = function () {
   location.href = "index.html";
 };
 document.getElementById("mobile-dropdown").addEventListener("click", function () {
-  const element = document.getElementById("home-mobile-dropdown");
+  const element = document.getElementById("mobile-dropdown-menu");
   element.classList.toggle("dropdown-hide");
 });
-const dropdownButton = document.getElementById('mobile-dropdown');
-const dropdownMenu = document.getElementById('home-mobile-dropdown');
-dropdownButton.addEventListener('click', () => {
-  dropdownMenu.classList.toggle('show');
-});
+

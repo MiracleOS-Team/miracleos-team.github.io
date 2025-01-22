fd6aef5280f6748df3c6feb9ef5853d15feb41dc
Teresa <teresanmegea@hotmail.com>
2025-01-22 18:21:32 -0300

    Made the buttons work on the dropdown

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ document.getElementById("desk-lrnmr-btn").onclick = function () {
 document.getElementById("home-mobile-btn").onclick = function () {
   location.href = "index.html";
 };
+document.getElementById("lrnmr-mobile-btn").onclick = function () {
+  location.href = "features/index.html";
+};
+document.getElementById("wiki-mobile-btn").onclick = function () {
+  location.href = "wiki/index.html";
+};
+document.getElementById("contrib-mobile-btn").onclick = function () {
+  location.href = "contribute/index.html";
+};
 document.getElementById("mobile-dropdown").addEventListener("click", function () {
   const element = document.getElementById("mobile-dropdown-menu");
   element.classList.toggle("dropdown-hide");

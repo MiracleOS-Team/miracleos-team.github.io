c06aa3b85905507b7b28c683e07ac3b14f6eb591
Teresa <teresanmegea@hotmail.com>
2025-01-22 14:28:31 -0300

    Added a dropdown on mobile

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-document.getElementById("download").onclick = function () {
+document.getElementById("desk-download-btn").onclick = function () {
   location.href = "download/index.html";
 };
-document.getElementById("learn-more").onclick = function () {
+document.getElementById("desk-lrnmr-btn").onclick = function () {
   location.href = "features/index.html";
 };
+document.getElementById("home-mobile-btn").onclick = function () {
+  location.href = "index.html";
+};
+document.getElementById("mobile-dropdown").addEventListener("click", function () {
+  const element = document.getElementById("home-mobile-dropdown");
+  element.classList.toggle("dropdown-hide");
+});
+const dropdownButton = document.getElementById('mobile-dropdown');
+const dropdownMenu = document.getElementById('home-mobile-dropdown');
+dropdownButton.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('show');
+});

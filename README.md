186ec399fb0e86ffb76471e40ac0721865c1ea3f
Zaka <zakahacecosas@protonmail.com>
2025-01-29 17:06:00 +0100

    Revamp website (commit 1/n)

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# MiracleOS official website
+# This is the repository for our website <https://miracleos.com>
 
-This is the repository for our website https://miracleos.com
-=======
-### MiracleOS's Website
-This is the the website of the project, which hosts the documentation, the download for the OS, and some ohter things
+## MiracleOS's Website
+
+This is the the website of the project, which hosts the documentation, the download for the OS, and some other things
--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-document.getElementById("desk-download-btn").onclick = function () {
-  location.href = "download/index.html";
-};
-document.getElementById("desk-lrnmr-btn").onclick = function () {
-  location.href = "features/index.html";
-};
-document.getElementById("home-mobile-btn").onclick = function () {
-  location.href = "index.html";
-};
-document.getElementById("lrnmr-mobile-btn").onclick = function () {
-  location.href = "features/index.html";
-};
-document.getElementById("wiki-mobile-btn").onclick = function () {
-  location.href = "wiki/index.html";
-};
-document.getElementById("contrib-mobile-btn").onclick = function () {
-  location.href = "contribute/index.html";
-};
-document.getElementById("mobile-dropdown").addEventListener("click", function () {
-  const element = document.getElementById("mobile-dropdown-menu");
-  element.classList.toggle("dropdown-hide");
-});
-
+document
+    .getElementById("mobile-dropdown")
+    .addEventListener("click", function () {
+        const element = document.getElementById("mobile-dropdown-menu");
+        element.classList.toggle("dropdown-hide");
+    });

3b8d76939697bf3373d1e74f14b2e7b3e0eb9ca8
SEG <86530914+SEGPlayz@users.noreply.github.com>
2025-02-21 16:44:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+var dropdownOpen = false
+
 document
     .getElementById("mobile-dropdown")
     .addEventListener("click", function () {
-        const element = document.getElementById("mobile-dropdown-menu");
-        element.classList.toggle("dropdown-hide");
-    });
+  if (dropdownOpen) {
+    closeDropdown()
+  } else {
+    openDropdown()
+  }
+})
+
+function openDropdown() {
+  document.getElementById("mobile-dropdown-menu").style.width = "100%";
+  dropdownOpen = true;
+}
+
+function closeDropdown() {
+  document.getElementById("mobile-dropdown-menu").style.width = "0";
+  dropdownOpen = false;
+}
